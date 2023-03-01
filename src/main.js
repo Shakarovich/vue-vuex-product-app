87c4934270d56fcaf50161a023c6027d65c1abbe
sigma <89007924+Shakarovich@users.noreply.github.com>
2023-03-01 16:03:18 +0500

    added state manager

--- src/main.js
+++ src/main.js
@@ -8,4 +8,5 @@ import store from "./store/index.js";
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
 app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,18 @@
 <template>
-  <pre>{{meals}}</pre>
+  <div class="flex flex-col p-8">
+    <input type="text"  class="rounded border-2 border-gray-200 w-full" placeholder="Search for meals"/>
+    <div class="flex gap-2 justify-center mt-2">
+      <router-link to="/" v-for="letter of letters">
+        {{letter}}
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import {computed} from 'vue';
 import store from '../store';
-  const meals = copmuted(() => store.state.meals)
+  const meals = computed(() => store.state.meals)
+  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("")
 </script>
 
