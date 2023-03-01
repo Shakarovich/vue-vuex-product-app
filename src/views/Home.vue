383b22fb23502b0cdfff58d06356dc3b51f30a58
sigma <89007924+Shakarovich@users.noreply.github.com>
2023-03-01 17:00:02 +0500

    fetch added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col p-8">
     <input type="text"  class="rounded border-2 border-gray-200 w-full" placeholder="Search for meals"/>
     <div class="flex gap-2 justify-center mt-2">
-      <router-link to="/" v-for="letter of letters">
+      <router-link :to="{name: 'byLetter', params: { letter }}" v-for="letter of letters" :key="letter">
         {{letter}}
       </router-link>
     </div>
@@ -10,9 +10,18 @@
 </template>
 
 <script setup>
-import {computed} from 'vue';
+import {computed, onMounted} from 'vue';
 import store from '../store';
-  const meals = computed(() => store.state.meals)
+import axiosClient from "../../axiosClient.js";
+import axios from 'axios';
+
   const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("")
+
+  onMounted(async( ) => {
+   axios.get('https://www.themealdb.com/api/json/v1/1/list.php?i=list')
+     .then((response) => {console.log(response)})
+     .catch((error) => {console.log(error)})
+  })
+
 </script>
 
