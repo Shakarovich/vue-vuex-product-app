4fb52b71a2410a49c092e8dd141b680a5f1376a3
sigma <89007924+Shakarovich@users.noreply.github.com>
2023-03-02 10:45:24 +0500

    added default-layout

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,6 @@
 <template>
   <div class="bg-gray-100 h-full">
-    <header class="bg-white shadow h-16 flex justify-between items-stretch">
-      <router-link :to="{name: 'home'}" class="inline-flex items-center h-full px-5 hover:bg-purple-100">
-        Home
-      </router-link>
-      <div>
-        <div class="flex items-center gap-2">
-          <router-link to="/" class="inline-flex items-center h-full px-5 hover:bg-purple-100" >
-            Search Meals
-          </router-link>
-          <router-link to="/" class="inline-flex items-center h-full px-5 hover:bg-purple-100">
-            Meals by letter
-          </router-link>
-          <router-link to="/" class="inline-flex items-center h-full px-5 hover:bg-purple-100">
-            Meals by gradients
-          </router-link>
-        </div>
-      </div>
-    </header>
+    <router-view/>
     <main>
       <router-view/>
     </main>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="bg-white shadow h-16 flex justify-between items-stretch">
+    <router-link :to="{name: 'home'}" class="inline-flex items-center h-full px-5 hover:bg-purple-100">
+      Home
+    </router-link>
+    <div>
+      <div class="flex items-center gap-2">
+        <router-link to="/" class="inline-flex items-center h-full px-5 hover:bg-purple-100" >
+          Search Meals
+        </router-link>
+        <router-link to="/" class="inline-flex items-center h-full px-5 hover:bg-purple-100">
+          Meals by letter
+        </router-link>
+        <router-link to="/" class="inline-flex items-center h-full px-5 hover:bg-purple-100">
+          Meals by gradients
+        </router-link>
+      </div>
+    </div>
+  </header>
+</template>
